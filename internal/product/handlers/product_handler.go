14a9aea8bf3f9b2a574cb23d5bdd0f7b113c46ca
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-25 23:22:33 +0530

    updated

--- internal/product/handlers/product_handler.go
+++ internal/product/handlers/product_handler.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"backend/internal/product/usecase"
+	usecases "backend/internal/product/usecase"
 	"backend/pkg/response"
 	"net/http"
 	"strconv"
@@ -13,38 +13,35 @@ type ProductHandler struct {
 	usecase *usecases.ProductUseCase
 }
 
-func NewProductHandler(u *usecases.ProductUseCase) *ProductHandler{
-   return &ProductHandler{u}
+func NewProductHandler(u *usecases.ProductUseCase) *ProductHandler {
+	return &ProductHandler{u}
 }
-
 
 // Get All Products
 
-func(h *ProductHandler) GetAllProducts(c *gin.Context){
+func (h *ProductHandler) GetAllProducts(c *gin.Context) {
 
-  products,err:=h.usecase.GetAllProducts()
-  if err!=nil {
-	response.InternalError(c,"Failed To Fetch Products")
-    return
- }
+	products, err := h.usecase.GetAllProducts()
+	if err != nil {
+		response.InternalError(c, "Failed To Fetch Products")
+		return
+	}
 
- c.JSON(200,products)
+	c.JSON(200, products)
 }
-
 
 // Get Products By Id
 
-func(h *ProductHandler)GetProductById(c *gin.Context){
+func (h *ProductHandler) GetProductById(c *gin.Context) {
 
-  id,_:=strconv.Atoi(c.Param("id")) 
+	id, _ := strconv.Atoi(c.Param("id"))
 
- products,err:= h.usecase.GetProductByID(uint(id))
- if err!=nil{
-   c.JSON(http.StatusNotFound,gin.H{"error":"Product Not Found"})
-   return
+	products, err := h.usecase.GetProductByID(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product Not Found"})
+		return
+	}
+
+	c.JSON(200, products)
+
 }
-
- c.JSON(200,products)
-
-}
-
