a0da3f2550572a9926e45b691df2c605c548fcc8
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-26 21:16:51 +0530

    updated

--- internal/auth/handler/auth_handler.go
+++ internal/auth/handler/auth_handler.go
@@ -8,39 +8,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
-
 type AuthHandler struct {
 	signupUC *usecase.SignupUseCase
 }
 
-func NewAuthHandler(signupUc *usecase.SignupUseCase) *AuthHandler{
-  
-  return &AuthHandler{signupUC: signupUc}
+func NewAuthHandler(signupUc *usecase.SignupUseCase) *AuthHandler {
+
+	return &AuthHandler{signupUC: signupUc}
 }
 
-func (h *AuthHandler)Signup(c *gin.Context){
+func (h *AuthHandler) Signup(c *gin.Context) {
 
-  var req dto.SignupRequest
+	var req dto.SignupRequest
 
- if err:= c.ShouldBindJSON(&req);err!=nil{
-  response.BadRequest(c,err.Error())
-  return
-}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
 
+	err := h.signupUC.Signup(req.Name, req.Email, req.Password)
 
- err:=h.signupUC.Signup(req.Name,req.Email,req.Password)
+	if err != nil {
+		if err == usecase.ErrEmailAlreadyExists {
+			response.Conflict(c, "email already exist")
+			return
+		}
 
- if err!=nil{
-   if err ==usecase.ErrEmailAlreadyExists{
-  response.Conflict(c,"email already exist")
-  return
-}
+		response.InternalError(c, "Server Error")
+		return
+	}
 
- response.InternalError(c,"Server Error")
- return
-}
-
-response.Created(c,"Signup Succesfully,verify otp",nil)
+	response.Created(c, "Signup Succesfully,verify otp", nil)
 
 }
