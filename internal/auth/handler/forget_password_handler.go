59d3e54cb35d8dbc8928bdc9a68e0446db10f77b
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-26 21:17:01 +0530

    updated

--- internal/auth/handler/forget_password_handler.go
+++ internal/auth/handler/forget_password_handler.go
@@ -11,27 +11,26 @@ type ForgetPasswordHandler struct {
 	usecase *usecase.ForgetPasswordUsecase
 }
 
-func NewForgetPasswordHandler(u *usecase.ForgetPasswordUsecase)*ForgetPasswordHandler{
- return &ForgetPasswordHandler{usecase: u}
+func NewForgetPasswordHandler(u *usecase.ForgetPasswordUsecase) *ForgetPasswordHandler {
+	return &ForgetPasswordHandler{usecase: u}
 }
 
-func(h *ForgetPasswordHandler)ForgetPassword(c *gin.Context){
+func (h *ForgetPasswordHandler) ForgetPassword(c *gin.Context) {
 
-  var req struct{
-   Email string `json:"email"`
+	var req struct {
+		Email string `json:"email"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Invalid Json")
+		return
+	}
+
+	if err := h.usecase.SendResendOTP(req.Email); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "OTP has Sent to renew Password"})
+
 }
-
-if err:=c.ShouldBindJSON(&req);err!=nil{
-   response.BadRequest(c,"Invalid Json")
-  return
-}
-
- if err:=h.usecase.SendResendOTP(req.Email);err!=nil{
-   response.BadRequest(c,err.Error())
-  return
-}
-
- c.JSON(200,gin.H{"message":"OTP has Sent to renew Password"})
- 
-}
-
