20921b105dfa1756e3b4ede8871088eb40b45785
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-26 21:17:21 +0530

    updated

--- internal/auth/handler/login_handler.go
+++ internal/auth/handler/login_handler.go
@@ -11,34 +11,31 @@ type LoginHandler struct {
 	usecase *usecase.LoginUsecase
 }
 
-func NewLoginHandler (u *usecase.LoginUsecase) *LoginHandler{
- return &LoginHandler{usecase: u}
+func NewLoginHandler(u *usecase.LoginUsecase) *LoginHandler {
+	return &LoginHandler{usecase: u}
 }
 
+func (h *LoginHandler) Login(c *gin.Context) {
 
+	var req struct {
+		Email    string `json:"email" binding:"required,email"`
+		Password string `json:"password" binding:"required"`
+	}
 
-func (h *LoginHandler)Login(c *gin.Context){
- 
- var req struct{
-  Email string `json:"email" binding:"required,email"`
-  Password string `json:"password" binding:"required"`
-}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Invalid request")
+		return
+	}
+	access, refresh, err := h.usecase.Login(req.Email, req.Password)
+	if err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
 
-  if err:=c.ShouldBindJSON(&req);err!=nil{
-    response.BadRequest(c,"Invalid request")
-    return
-}
-  access,refresh,err:=h.usecase.Login(req.Email,req.Password);
- if err!=nil{
-   response.BadRequest(c,err.Error())
-   return
-}
-
-  
- c.JSON(200,gin.H{
-    "message":"Login Successfull",
-    "access_token":access,
-    "refresh_token" : refresh,
-})
+	c.JSON(200, gin.H{
+		"message":       "Login Successfull",
+		"access_token":  access,
+		"refresh_token": refresh,
+	})
 
 }
