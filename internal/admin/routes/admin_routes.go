b314a559d4e2920d2b00a15e7eaa96a5321341aa
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-26 21:15:54 +0530

    updated

--- internal/admin/routes/admin_routes.go
+++ internal/admin/routes/admin_routes.go
@@ -7,18 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AdminRoutes(r *gin.Engine,h *admin_handler.UserAdminHandler,p *admin_handler.ProductAdminHandler){
+func AdminRoutes(r *gin.Engine, h *admin_handler.UserAdminHandler, p *admin_handler.ProductAdminHandler) {
 
- admin := r.Group("/admin")
- admin.Use(middleware.JWTAuth(),middleware.AdminOnly())
+	admin := r.Group("/admin")
+	admin.Use(middleware.JWTAuth(), middleware.AdminOnly())
 
- users := admin.Group("/users")
- users.PUT("/:id",h.UpdateUser)
- users.PUT("/:id/block",h.BlockUser)
+	users := admin.Group("/users")
+	users.PUT("/:id", h.UpdateUser)
+	users.PUT("/:id/block", h.BlockUser)
 
- products:=admin.Group("/products")
- products.POST("",p.CreateProduct)
- products.PUT("/:id",p.UpdateProduct)
- products.DELETE("/:id",p.DeleteProduct)
- 
+	products := admin.Group("/products")
+	products.POST("", p.CreateProduct)
+	products.PUT("/:id", p.UpdateProduct)
+	products.DELETE("/:id", p.DeleteProduct)
+
 }
