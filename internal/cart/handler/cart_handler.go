8c4349f1130c53a5657f5bcce33fc66091f5590b
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-01-29 13:13:43 +0530

    updated

---
+++ internal/cart/handler/cart_handler.go
@@ -0,0 +1,92 @@
+package cart_handler
+
+import (
+	cart_usecase "backend/internal/cart/usecase"
+	"backend/pkg/response"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CartHandler struct {
+	usecase *cart_usecase.CartUsecase
+}
+
+func NewCartHandler(usecase *cart_usecase.CartUsecase) *CartHandler {
+	return &CartHandler{usecase}
+}
+
+func (h *CartHandler) Add(c *gin.Context) {
+
+	userID := c.GetUint("user_id")
+
+	idParam := c.Param("product_id")
+	productID, err := strconv.Atoi(idParam)
+	if err != nil {
+		response.BadRequest(c, "Invalid Product ID")
+		return
+	}
+
+	if err := h.usecase.Add(userID, uint(productID)); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
+	c.JSON(200, gin.H{"message": "Product Added To Cart"})
+}
+
+func (h *CartHandler) Remove(c *gin.Context) {
+
+	userID := c.GetUint("user_id")
+
+	idParam := c.Param("product_id")
+	productID, err := strconv.Atoi(idParam)
+	if err != nil {
+		response.BadRequest(c, "Invalid Product ID")
+		return
+	}
+
+	if err := h.usecase.Remove(userID, uint(productID)); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Product removed From Cart"})
+
+}
+
+func (h *CartHandler) UpdateQty(c *gin.Context) {
+
+	type updarequantity struct {
+		Quantity int `json:"quantity"`
+	}
+
+	userID := c.GetUint("user_id")
+	idParam := c.Param("product_id")
+
+	productID, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		response.BadRequest(c, "Invalid Product ID")
+		return
+	}
+
+	var req updarequantity
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Ivavlid JSON")
+		return
+	}
+
+	if req.Quantity < 0 {
+		response.BadRequest(c, "Quantity Cannot be negative")
+		return
+	}
+
+	if err := h.usecase.UpdateQty(userID, uint(productID), req.Quantity); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Quantity Updated"})
+
+}
---
+++ internal/cart/usecase/cart_usecase.go
@@ -0,0 +1,70 @@
+package cart_usecase
+
+import (
+	cart_entity "backend/internal/cart/entity"
+	cart_repository "backend/internal/cart/repository"
+	"errors"
+)
+
+type CartUsecase struct {
+	repo cart_repository.CartRepository
+}
+
+func NewCartUsecase(repo cart_repository.CartRepository) *CartUsecase {
+	return &CartUsecase{repo}
+}
+
+func (u *CartUsecase) Add(userID, productID uint) error {
+
+	if productID == 0 {
+		return errors.New("Invalid Product ID")
+	}
+
+	return u.repo.Add(userID, productID)
+
+}
+
+func (u *CartUsecase) Remove(userID, productID uint) error {
+	if productID == 0 {
+		return errors.New("Invalid Product ID")
+	}
+
+	return u.repo.Remove(userID, productID)
+
+}
+
+func (u *CartUsecase) UpdateQty(userID, productID, qty uint) error {
+
+	if qty == 0 {
+		return u.repo.Remove(userID, productID)
+	}
+
+	return u.repo.UpdateQty(userID, productID, qty)
+
+}
+
+func (u *CartUsecase) Clear(userID uint) error {
+
+	return u.repo.Clear(userID)
+}
+
+func (u *CartUsecase) GetMyUser(userID uint) ([]cart_entity.Cart, error) {
+
+	return u.repo.GetByUser(userID)
+
+}
+
+func (u *CartUsecase) ValidateCart(userID uint) error {
+
+	items, err := u.repo.GetByUser(userID)
+	if err != nil {
+		return err
+	}
+
+	if len(items) == 0 {
+		return errors.New("Cart is Empty")
+	}
+
+	return nil
+
+}
