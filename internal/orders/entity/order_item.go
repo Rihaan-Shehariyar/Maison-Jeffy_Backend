18440e85e83dc9a2c0f93befc87d1d48cdcb9040
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-01 23:38:42 +0530

    updated

--- internal/orders/entity/order_item.go
+++ internal/orders/entity/order_item.go
@@ -1,11 +1,9 @@
 package order_entity
 
 type OrderItem struct {
-
 	ID        uint `gorm:"primaryKey"`
 	OrderID   uint `gorm:"not null;index"`
 	ProductID uint
 	Price     float64
 	Quantity  uint
-
 }
