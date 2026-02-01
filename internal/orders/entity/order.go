2bef9a7d14b953da867ab45c832611f2cb67abf3
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-01 23:38:58 +0530

    updated

--- internal/orders/entity/order.go
+++ internal/orders/entity/order.go
@@ -3,10 +3,12 @@ package order_entity
 import "time"
 
 type Order struct {
+
 	ID          uint `gorm:"primaryKey"`
 	UserID      uint `gorm:"index"`
 	TotalAmount float64
 	Status      string
 	CreatedAt   time.Time
 	OrderItems  []OrderItem `gorm:"foreignKey:OrderID"`
+
 }
