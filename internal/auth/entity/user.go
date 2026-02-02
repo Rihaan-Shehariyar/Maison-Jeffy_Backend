4c89660a19ca1a9475cc0fdc435386d0f969945d
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-02 20:52:00 +0530

    updated

--- internal/auth/entity/user.go
+++ internal/auth/entity/user.go
@@ -3,14 +3,12 @@ package entity
 import "time"
 
 type User struct {
-
-	ID         uint      `gorm:"primaryKey"`
+	ID         uint `gorm:"primaryKey"`
 	Name       string
-	Email      string    `gorm:"uniqueIndex;not null"`
-	Password   string    `gorm:"not null"`
-	Role       string      `gorm:"default:user"`
-	IsVerified bool      `gorm:"default:false"`
-    IsBlocked bool       `gorm:"default:false"`
+	Email      string `gorm:"uniqueIndex;not null"`
+	Password   string `gorm:"not null"`
+	Role       string `gorm:"default:user"`
+	IsVerified bool   `gorm:"default:false"`
+	IsBlocked  bool   `gorm:"default:false"`
 	CreatedAt  time.Time
-
 }
