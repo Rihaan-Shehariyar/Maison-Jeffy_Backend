80410bf13b917b4c7eeefcf0ad488032049ed7b1
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-02 20:53:40 +0530

    updated

--- internal/auth/entity/otp.go
+++ internal/auth/entity/otp.go
@@ -3,12 +3,10 @@ package entity
 import "time"
 
 type OTP struct {
-
 	ID        uint   `gorm:"primaryKey"`
 	Email     string `gorm:"index"`
 	CodeHash  string
-	ExpiresAt time.Time    
-    Used bool  `gorm:"default:false"`
-    CreatedAt time.Time
-
+	ExpiresAt time.Time
+	Used      bool `gorm:"default:false"`
+	CreatedAt time.Time
 }
