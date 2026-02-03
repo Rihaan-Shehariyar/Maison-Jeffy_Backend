7ee0fc6a9a332895a61d70c9345cceaf2d071973
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-03 16:36:08 +0530

    updated

--- internal/auth/handler/reset_password_handler.go
+++ internal/auth/handler/reset_password_handler.go
@@ -11,32 +11,28 @@ type ResetPasswordHandler struct {
 	usecase *usecase.ResetPasswordUsecase
 }
 
-func NewResetPasswordHandler(u *usecase.ResetPasswordUsecase) *ResetPasswordHandler{
- return &ResetPasswordHandler{usecase: u }
+func NewResetPasswordHandler(u *usecase.ResetPasswordUsecase) *ResetPasswordHandler {
+	return &ResetPasswordHandler{usecase: u}
 }
 
+func (h *ResetPasswordHandler) ResetPassword(c *gin.Context) {
 
-func (h *ResetPasswordHandler) ResetPassword(c *gin.Context){
+	var req struct {
+		Email       string `json:"email"`
+		OTP         string `json:"otp"`
+		NewPassword string `json:"new_password"`
+	}
 
-  var req struct{
-   Email string `json:"email"`
-   OTP string `json:"otp"`
-   NewPassword string `json:"new_password"`
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Invalid Json")
+		return
+	}
+
+	if err := h.usecase.ResetPassword(req.Email, req.OTP, req.NewPassword); err != nil {
+		response.BadRequest(c, err.Error())
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "Password Reset Successfully"})
+
 }
-
- if err:=c.ShouldBindJSON(&req);err!=nil{
-  response.BadRequest(c,"Invalid Json")
- return
-}
-
- if err:=h.usecase.ResetPassword(req.Email,req.OTP,req.NewPassword);err!=nil{
-    response.BadRequest(c,err.Error())
-   return
-}
-
- c.JSON(200,gin.H{"message":"Password Reset Successfully"})
-
-}
-
-
-
