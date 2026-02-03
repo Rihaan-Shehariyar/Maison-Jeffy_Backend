c2b8e48dc0fc17e7df82d8671c480934438d4373
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-03 16:36:47 +0530

    updated

--- internal/auth/repository/otp_repository.go
+++ internal/auth/repository/otp_repository.go
@@ -8,42 +8,37 @@ import (
 
 type OtpRepository interface {
 	Create(otp *entity.OTP) error
-    FindLatestByEmail(email string)(*entity.OTP,error)
-    MarkUsed(id uint)error
+	FindLatestByEmail(email string) (*entity.OTP, error)
+	MarkUsed(id uint) error
 }
 
-type otpRepository struct{
- 
-  db *gorm.DB
-  
+type otpRepository struct {
+	db *gorm.DB
 }
 
-
-func NewOTPRepository (db *gorm.DB) OtpRepository {
-  return &otpRepository{db}
+func NewOTPRepository(db *gorm.DB) OtpRepository {
+	return &otpRepository{db}
 }
 
-func (r *otpRepository)Create(otp *entity.OTP)error{
-    return r.db.Create(otp).Error
+func (r *otpRepository) Create(otp *entity.OTP) error {
+	return r.db.Create(otp).Error
 }
 
-func (r *otpRepository)FindLatestByEmail(email string)(*entity.OTP,error){
+func (r *otpRepository) FindLatestByEmail(email string) (*entity.OTP, error) {
 
-  var otp entity.OTP
+	var otp entity.OTP
 
- err:=r.db.Where("email = ? AND used=false",email).
-      Order("created_at DESC").
-      First(&otp).Error
-  return &otp,err
+	err := r.db.Where("email = ? AND used=false", email).
+		Order("created_at DESC").
+		First(&otp).Error
+	return &otp, err
 
 }
 
-func (r *otpRepository)MarkUsed(id uint)error{
-  
-  return r.db.Model(&entity.OTP{}).
-          Where("id = ?",id).
-          Update("used",true).Error
+func (r *otpRepository) MarkUsed(id uint) error {
 
- }
+	return r.db.Model(&entity.OTP{}).
+		Where("id = ?", id).
+		Update("used", true).Error
 
-
+}
