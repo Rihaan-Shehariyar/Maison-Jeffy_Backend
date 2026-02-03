29a6a4b80b8ca855561db05580f738de7cef8660
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-03 16:35:45 +0530

    updated

--- internal/auth/handler/refresh_handler.go
+++ internal/auth/handler/refresh_handler.go
@@ -10,27 +10,27 @@ import (
 type RefreshHandler struct {
 	usecase *usecase.RefreshUseCase
 }
-func NewRefreshHandler(u *usecase.RefreshUseCase)*RefreshHandler{
-  return &RefreshHandler{usecase: u}
+
+func NewRefreshHandler(u *usecase.RefreshUseCase) *RefreshHandler {
+	return &RefreshHandler{usecase: u}
 }
 
-func(h *RefreshHandler)Refresh(c *gin.Context){
-   var req struct{
-    RefreshToken string `json:"refresh_token"`
+func (h *RefreshHandler) Refresh(c *gin.Context) {
+	var req struct {
+		RefreshToken string `json:"refresh_token"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Invalid Json Format")
+		return
+	}
+
+	access_token, err := h.usecase.Refresh(req.RefreshToken)
+	if err != nil {
+		response.Unauthorized(c, err.Error())
+		return
+	}
+
+	c.JSON(200, gin.H{"access_token": access_token})
+
 }
-
- if err:=c.ShouldBindJSON(&req);err!=nil{
-    response.BadRequest(c,"Invalid Json Format")
-     return
-}
-
- access_token,err:=h.usecase.Refresh(req.RefreshToken) 
- if err!=nil{
-    response.Unauthorized(c,err.Error())
-    return
-}
-
- c.JSON(200,gin.H{"access_token" : access_token})
-
-}
-
