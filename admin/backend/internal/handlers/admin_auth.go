67757e9323d7c0c03628a056c7064017ed53184f
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-06 17:12:10 +0530

    updated

---
+++ admin/backend/internal/handlers/admin_auth.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"backend/admin/backend/internal/database"
+	"backend/admin/backend/internal/models"
+	jwtutils "backend/pkg/jwt_utils"
+	"backend/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminLogin(c *gin.Context) {
+
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		response.BadRequest(c, "Invalid Json")
+		return
+
+	}
+
+ var admin  models.Admin
+
+ err:= database.DB.Where("email = ? AND password = ?",req.Email,req.Password).First(&admin).Error
+
+ if err!=nil{
+  response.Unauthorized(c,"Invalid Credentials")
+  return
+}
+
+ token,err:=jwtutils.GenerateAccessToken(admin.ID,admin.Email,"admin")
+if err!=nil{
+ response.InternalError(c,"Token Generation Failed")
+ return
+}
+
+ 
+
+c.JSON(200,gin.H{"token":token})
+
+}
