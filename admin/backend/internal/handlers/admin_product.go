91c41f2f5834bc9e47fb5bebb137532d20d55d44
Rihaan Shehariyar <rihaanshehariyar@gmail.com>
2026-02-06 18:10:23 +0530

    updated

--- admin/backend/internal/handlers/admin_product.go
+++ admin/backend/internal/handlers/admin_product.go
@@ -3,15 +3,90 @@ package handlers
 import (
 	"backend/admin/backend/internal/database"
 	entitys "backend/internal/product/entity"
+	"backend/pkg/response"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetAllProducts(c *gin.Context){
- 
- var products []entitys.Product
- database.DB.Find(&products)
- c.JSON(200,products)
+func GetAllProducts(c *gin.Context) {
+
+	var products []entitys.Product
+	database.DB.Find(&products)
+	c.JSON(200, products)
 
 }
 
+func CreateProduct(c *gin.Context) {
+
+	name := c.PostForm("name")
+	priceStr := c.PostForm("price")
+	stockstr := c.PostForm("stock")
+	description := c.PostForm("category")
+	category := c.PostForm("category")
+
+	if name == "" || priceStr == "" || stockstr == "" {
+		response.BadRequest(c, "Name,price and Stock is required")
+		return
+	}
+
+	price, err := strconv.ParseFloat(priceStr, 64)
+	if err != nil || price < 0 {
+		response.BadRequest(c, "Invalid Price")
+		return
+	}
+
+	stock, err := strconv.Atoi(stockstr)
+	if err != nil || stock < 0 {
+		response.BadRequest(c, "Invalid Price")
+		return
+	}
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		response.BadRequest(c, "Image is required")
+		return
+	}
+
+	uploadDir := "uploads/products"
+	_ = os.Mkdir(uploadDir, os.ModePerm)
+
+	fileName := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
+	imagePath := filepath.Join(uploadDir, fileName)
+
+	if err := c.SaveUploadedFile(file, imagePath); err != nil {
+		response.InternalError(c, "Failed to Save Image")
+		return
+	}
+
+	product := entitys.Product{
+		Name:        name,
+		Description: description,
+		Price:       price,
+		Stock:       stock,
+		ImageURL:    imagePath,
+		Category:    category,
+	}
+
+	if err := database.DB.Create(&product).Error; err != nil {
+		response.InternalError(c, "Failed To create Product")
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Product Created Succeesfullt",
+		"product": product,
+	})
+
+}
+
+func DeleteProduct(c *gin.Context){
+
+ database.DB.Delete(&entitys.Product{},c.Param("id"))
+ c.JSON(200,gin.H{"message":"Product Deleted Successfully"})
+
+}
